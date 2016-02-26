f07452ea5bc172115f282b3eb4bb46bb9cfdfb6f
Jeswin Kumar <jeswinpk@agilehead.com>
2016-02-26 21:29:16 +0530

    Make render() async

--- src/isotropy-adapter-webapp.js
+++ src/isotropy-adapter-webapp.js
@@ -6,14 +6,14 @@ export type RenderArgsType = {
   req: IncomingMessage,
   res: ServerResponse,
   args: Object,
-  handler: (req: IncomingMessage, res: ServerResponse, args: Object) => void,
+  handler: (req: IncomingMessage, res: ServerResponse, args: Object) => Promise,
   toHtml?: (html: string, props: Object) => string
 }
 
-const render = function(params: RenderArgsType) : void {
+const render = async function(params: RenderArgsType) : Promise {
   const { req, res, args, handler, toHtml } = params;
   const dummyResponse = new Response({ res, args, toHtml });
-  handler(req, dummyResponse, args);
+  return handler(req, dummyResponse, args);
 };
 
 export default {
